3571bd9befd65bb776ee07aea3cc1d4f22a58ed0
Rxnai23 <mahawan2549@gmail.com>
2024-08-16 13:58:19 +0700

    o

--- LAB6.cpp
+++ LAB6.cpp
@@ -1,3 +1,30 @@
 #include <stdio.h>
-ht  htdglbmskot]epebmktp
-hghghg
+    int main () {
+
+        int number ;
+
+        printf("Enter Your Number : ") ;
+        scanf( "%d", &number ) ;
+        for( int i = 0 ; i < number ; i++ ) {
+            if( number %2 == 0 ) {
+                for(int column = 0 ; column < number ; column++ ) {
+                    if( i == column ) {
+                        printf("1") ;
+                    } else {
+                        printf("0") ;
+                    }
+                }
+            } else {
+                for( int column = 0 ; column < number ; column++ ){
+                    if( i + column == number - 1 ) {
+                        printf("1")
+                    } else {
+                        printf("0")
+                    }
+                }
+            }
+            printf("\n")
+        }
+        return 0 ;
+    }
+    
