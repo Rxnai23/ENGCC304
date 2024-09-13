0c737e9aac9757c8c1689fd3b5306da3b2ac5056
Rxnai23 <mahawan2549@gmail.com>
2024-09-13 09:47:07 +0700

    ....

---
+++ LAB9 .cpp
@@ -0,0 +1,39 @@
+/*
+    จงเขียนโปรแกรมรับค่าจากผู้ใช้เข้าไปใส่ในอาเรย์และแสดงผลลัพธ์ออกมาทั้งหมดที่เป็นจำนวนเฉพาะเท่านั้น (ค่าที่ไม่ใช่จำนวนเฉพาะแสดงเครื่องหมาย # แทน)
+
+    Test case:
+        Enter N :
+            6
+        Enter value[0] :
+            19
+        Enter value[1] :
+            18
+        Enter value[2] :
+            27
+        Enter value[3] :
+            15
+        Enter value[4] :
+            4
+        Enter value[5] :
+            2
+    Output:
+        Index:  0  1  2  3  4  5
+        Array:  # 19  #  #  #  2
+*/
+#include <stdio.h>
+
+int main () {
+
+    int num[6], i ;
+
+    printf("Enter N :") ;
+    scanf("%d", &num) ;
+
+    int value[num] ;
+    int i = 0 ;
+
+    for (i = 0; i < num ; i++) {
+        printf("Enter value[]")
+    }
+    
+}
